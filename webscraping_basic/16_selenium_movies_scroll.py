93aaa1a30df80237a57a448e0c9dede78c87956d
DevHam94 <gkaeogus7@gmail.com>
2024-12-07 09:44:48 +0900

    241207

--- webscraping_basic/16_selenium_movies_scroll.py
+++ webscraping_basic/16_selenium_movies_scroll.py
@@ -21,16 +21,36 @@
 #   title = movie.find("div", attrs={"class":""}).get_text()
 #   print(title)
 
+
 from selenium import webdriver
-browser = webdrvier.Chrome()
-browser.maximize.window()
+browser = webdriver.Chrome()
+browser.maximize_window()
 
 # 페이지 이동 
 url = "https://play.google.com/store/movies?hl=ko&pli=1"
 browser.get(url)
- 
+
+# 지정한 위치로 스크롤 내리기 
 # 스크롤 내리기
-browser.execute_script("window.scrollTo(0, 1080)")
- 
- 
- 
+# 모니터(해상도) 높이인 1080 위치로 s스크롤 내리기 
+# browser.execute_script("window.scrollTo(0, 1080)") # 1920 x 1080
+# browser.execute_script("window.scrollTo(0, 2080)") 
+
+# 화면 가장 아래로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+import time
+interval = 2 # 2초에 한번씩 스크롤 내림
+
+# 현재 문서 높이를 가져와서 저장
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+# 반복 수행
+while True:
+  # 스크롤을 가장 아래로 내림
+  browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+  #페이지 로딩 대기
+
+  # 현재 문서 높이를 가져와서 저장
+  curr_height = browser.execute_script
